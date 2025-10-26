fdeee8f96a8152d56a01310898e4babc44de40c3
sh7478 <sh7478@bs.amalnet.k12.il>
2025-10-26 19:14:43 +0200

    Regular class finished

---
+++ app/src/main/java/com/example/ex0241/ex2/Regular.java
@@ -0,0 +1,30 @@
+package com.example.ex0241.ex2;
+
+public class Regular extends Vehicles{
+    private int passengerAmount;
+
+    public Regular(int carNumber, double carAge, int wheelsAmount, String steeringType, double exhaustPullotPerMin, int passengerAmount) {
+        super(carNumber, carAge, wheelsAmount, steeringType, exhaustPullotPerMin);
+        this.passengerAmount = passengerAmount;
+    }
+
+    public int getPassengerAmount() {
+        return passengerAmount;
+    }
+
+    public void setPassengerAmount(int passengerAmount) {
+        this.passengerAmount = passengerAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Heavy{" +
+                "passengerAmount=" + this.passengerAmount +
+                "carNumber=" + super.getCarNumber() +
+                ", carAge=" + super.getCarAge() +
+                ", wheelsAmount=" + super.getWheelsAmount() +
+                ", steeringType='" + super.getSteeringType() + '\'' +
+                ", exhaustPullotPerMin=" + super.getExhaustPullotPerMin() +
+                '}';
+    }
+}
