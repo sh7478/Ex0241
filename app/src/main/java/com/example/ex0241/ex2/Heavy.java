eec9eb84647e9ffa94e50f9ae5696666efd047c0
sh7478 <sh7478@bs.amalnet.k12.il>
2025-10-26 19:08:55 +0200

    Heavy class finished

---
+++ app/src/main/java/com/example/ex0241/ex2/Heavy.java
@@ -0,0 +1,29 @@
+package com.example.ex0241.ex2;
+
+public class Heavy extends Vehicles{
+    private int trailersAmount;
+    public Heavy(int carNumber, double carAge, int wheelsAmount, String steeringType, double exhaustPullotPerMin, int trailersAmount) {
+        super(carNumber, carAge, wheelsAmount, steeringType, exhaustPullotPerMin);
+        this.trailersAmount = trailersAmount;
+    }
+
+    public int getTrailersAmount() {
+        return trailersAmount;
+    }
+
+    public void setTrailersAmount(int trailersAmount) {
+        this.trailersAmount = trailersAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Heavy{" +
+                "trailersAmount=" + trailersAmount +
+                "carNumber=" + super.getCarNumber() +
+                ", carAge=" + super.getCarAge() +
+                ", wheelsAmount=" + super.getWheelsAmount() +
+                ", steeringType='" + super.getSteeringType() + '\'' +
+                ", exhaustPullotPerMin=" + super.getExhaustPullotPerMin() +
+                '}';
+    }
+}
